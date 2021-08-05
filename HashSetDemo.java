ca22d73f99f1710ffd88545b05d1b880a72acc5d
ashaqmaqbool68 <47243055+ashaqmaqbool68@users.noreply.github.com>
2021-08-04 23:14:48 -0700

    Add files via upload

---
+++ HashSetDemo.java
@@ -0,0 +1,12 @@
+package basics;
+import java.util.HashSet;
+public class HashSetDemo {
+ public static void main(String args[]){   
+HashSet<String> set=new HashSet<String>();  
+set.add("Rahi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");
+System.out.println(set);
+}
+}
